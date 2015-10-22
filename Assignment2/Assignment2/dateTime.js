b2e1222ced2a9f1f6bc13fea7caf55cba6739fb4
Manpreet Singh Matharu <matharumanpreet00@gmail.com>
2015-10-22 10:42:12 -0400

    Added Assignent 2

---
+++ Assignment2/Assignment2/dateTime.js
@@ -0,0 +1,18 @@
+﻿
+var date = new Date();
+var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+function dayCreate(day) {
+    var assign;
+    var currentDate;
+    if (day === 1 || day === 21 || day === 31)
+        assign = "st";
+    else if (day === 3 || day === 23)
+        assign = "rd";
+    else
+        assign = "th";
+    currentDate = day + assign;
+    return currentDate;
+}
+var currentMonth = months[date.getMonth()];
+
+
